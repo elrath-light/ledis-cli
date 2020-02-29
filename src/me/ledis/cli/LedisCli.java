c2dea8b98b6e3f9151b1e627a4569bacbc9aeee7
elrath-light <20616561+elrath-light@users.noreply.github.com>
2020-03-01 01:39:50 +0700

    Add jar and manifest, rename main class

--- src/me/ledis/cli/LedisCli.java
+++ src/me/ledis/cli/LedisCli.java
@@ -7,11 +7,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class Main {
+public class LedisCli {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            System.out.print("Enter your command: ");
             String command = scanner.nextLine();
             if (command.equals("exit")) {
                 break;
